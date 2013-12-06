528a0951aadb64798019267d33969b2af33d4753
Jon Saints <saintsjd@gmail.com>
2013-12-06 10:49:48 -0700

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('imiJobsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
