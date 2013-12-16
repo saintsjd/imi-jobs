73bafa4438f89bffbd1bb0cb9f8f1f66d8bde931
Jon Saints <saintsjd@gmail.com>
2013-12-16 15:29:05 -0700

    instructions for case when there are no saved reports

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,13 +1,14 @@
 'use strict';
 
 angular.module('imiJobsApp')
-  .controller('MainCtrl', ['$scope','$location','auth','jobs', function ($scope, $location, auth, jobs) {
+  .controller('MainCtrl', ['$scope','$location','auth','$timeout','jobs', function ($scope, $location, auth, $timeout, jobs) {
 
     if( !auth.isAuthenticated() ){
       $location.path('/login');
     }
 
-    $scope.jobs = jobs;
+    $scope.jobs = jobs.jobs;
     $scope.token = auth.token;
+
 
   }]);
--- app/scripts/services/jobs.js
+++ app/scripts/services/jobs.js
@@ -1,10 +1,37 @@
 'use strict';
 
 angular.module('imiJobsApp')
-	.factory('jobs', function() {
-		return [
-			{name:'job1'},
-			{name:'job2'},
-			{name:'job3'}
-		];
-	});
+  .factory('jobs', ['$timeout', function( $timeout ) {
+
+    var jobs = [];
+
+    /*$timeout( function(){
+      jobs.push(
+        {
+          id: 1,
+          description:'North Dakota Expansion'
+        }
+      );
+      jobs.push(
+        {
+          id: 2,
+          description:'Chicago Prospect List'
+        }
+      );
+      jobs.push(
+        {
+          id: 3,
+          description:'Future Branch Locations'
+        }
+      );
+      console.log(jobs);
+    }, 4000);*/
+
+
+    return {
+      jobs: jobs,
+      getJobs: function() {
+        return jobs;
+      },
+    };
+  }]);
