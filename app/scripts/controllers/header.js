85020264076df921d6897b077d6c984f5f935efa
Jon Saints <saintsjd@gmail.com>
2013-12-16 00:17:14 -0700

    cookie storage for user and logout

--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -1,9 +1,14 @@
 'use strict';
 
 angular.module('imiJobsApp')
-  .controller('HeaderCtrl', ['$scope','auth', function ($scope, auth ) {
+  .controller('HeaderCtrl', ['$scope','auth','$location', function ($scope, auth, $location ) {
 
     $scope.loggedIn = auth.isAuthenticated;
     $scope.user = auth.user;
+
+    $scope.logout = function(){
+        auth.logout();
+        $location.path('/#/login');
+      };
 
   }]);
--- app/scripts/services/auth.js
+++ app/scripts/services/auth.js
@@ -1,23 +1,25 @@
 'use strict';
 
 angular.module('imiJobsApp')
-  .factory('auth', function () {
+  .factory('auth', ['$cookieStore',function ($cookieStore) {
 
-    var user = {'email':null,'token':null};
+    var user = $cookieStore.get('user') || {'email':null,'token':null};
 
     return {
       user: user,
       login: function(email,password){
         user.email = email;
         user.token = 'TOKEN_HERE'+password;
+        $cookieStore.put('user',user);
       },
       logout: function(){
         user.email = null;
         user.token = null;
+        $cookieStore.remove('user',user);
       },
       isAuthenticated: function(){
         return !!user.token;
       },
     };
 
-  });
+  }]);
